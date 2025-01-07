cc225fc7dcecd359b9100154d736a7e1899de5fe
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2025-01-07 00:48:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Chat En tiempo Real
+
+En este proyecto de javaScript vanila, se conecta con un socket y un server APIRest
+Para traer la lista de contactos, los chats de cada contacto y enviar mensajes
+
+## Antes De empezar
+Coloque la url del servidor, ya sea que este en la nuve, o que use otro método
---
+++ src/index.js
@@ -0,0 +1,148 @@
+const listContacts = document.querySelector('.list-contact');
+const contentChat = document.querySelector('.content-chat-history');
+
+const formMessage = document.querySelector('.frm-message');
+
+const chatHtml = document.getElementById('chat');
+
+let contactFocus = null;
+
+let BaseUrl = null;
+
+const setBaseUrl = ()=>{
+
+  if (localStorage.getItem('BaseUrl')) {
+    BaseUrl = localStorage.getItem('BaseUrl');
+    return true;
+  }
+
+  BaseUrl = prompt('Añadir el link del back');
+  if(BaseUrl){
+    localStorage.setItem('BaseUrl', BaseUrl);
+    return true;
+  }
+
+  return false;
+}
+
+
+async function traerChat(phoneNumber){
+  const chat = await fetch(`${BaseUrl}/api/whatsapp/chats/${phoneNumber}`).then( response => response.json() );
+  showChat( chat.messages );
+}
+
+async function traerListContact(){
+  const contacts = await fetch(`${BaseUrl}/api/whatsapp/contacts`).then( response => response.json() );
+  showListContacts( contacts );
+}
+
+function showListContacts(contacts){
+  listContacts.innerHTML = '';
+  contacts.forEach(contact => {
+    const lastMessage = contact.lastMessage;
+
+    const message = (lastMessage.of === 'me')
+      ? lastMessage.text
+      : `${contact.nameContact}: ${lastMessage.text}`;
+
+    const li = document.createElement('li');
+    li.classList.add('contact');
+    li.classList.remove('chat-selected');
+    li.innerHTML = `
+      <div class="icon-contact"> M </div>
+      <div class="name-contact">
+          <h2>${contact.nameContact}</h2>
+          <div>${message}</div>
+      </div>
+    `;
+
+    li.addEventListener('click', ()=>{
+      li.classList.add('chat-selected');
+      contentChat.style.display = 'grid';
+      contactFocus = contact;
+      traerChat(contact.phoneNumber)
+    })
+    listContacts.appendChild(li);
+  });
+}
+
+function showChat( messages ){
+  chatHtml.innerHTML = '';
+  messages.forEach((m)=>{
+    const contentMessage = document.createElement('div');
+    contentMessage.classList.add('content-message');
+
+    const message = document.createElement('div');
+    contentMessage.appendChild(message);
+    message.classList.add('message');
+
+    if(m.of === 'me'){
+      contentMessage.style.justifyContent = 'flex-end'
+      message.classList.add('message-me');
+    }
+    if(m.of === 'to') message.classList.add('message-contact');
+
+    message.textContent = m.text;
+    chatHtml.appendChild(contentMessage);
+  });
+}
+
+async function sendMessage( to, message ){
+  console.log(to, message)
+  await fetch(`${BaseUrl}/api/whatsapp/sendMessage`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json', // Indica que el body está en formato JSON
+    },
+    body: JSON.stringify({ to, message }) 
+  }).then( response => true );
+}
+
+function connectToWebSockets() {
+
+  if (!setBaseUrl()){
+    alert('Ingrese el enpoint');
+    return;
+  }
+
+  const socket = new WebSocket( `ws://${BaseUrl}/ws` );
+
+  if(socket.readyState == 0) return alert('Hubo un error, puede que la url no sea la correcta, revise bien porfavor');
+
+  traerListContact();
+  socket.onmessage = ( event ) => {
+    const { type, payload } = JSON.parse(event.data);
+    if(type === 'on-contact-new'){
+      showListContacts( payload );
+    }
+
+    if(type === 'on-chat-changed'){
+      if (payload.phoneNumber === contactFocus.phoneNumber) {
+        showChat( payload.messages );
+      }
+    }
+  };
+
+  socket.onclose = ( event ) => {
+    console.log( 'Connection closed' );
+    setTimeout( () => {
+      console.log( 'retrying to connect' );
+      connectToWebSockets();
+    }, 1500 );
+  };
+
+  socket.onopen = ( event ) => {
+    console.log( 'Connected' );
+  };
+}
+
+formMessage.addEventListener('submit', async(e)=>{
+  e.preventDefault();
+  const inputMessage = e.target[1];
+
+  await sendMessage(contactFocus.phoneNumber, inputMessage.value);
+
+  inputMessage.value = '';
+})
+
+connectToWebSockets();
