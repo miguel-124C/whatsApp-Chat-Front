6e885ec1043925959284acbe3d11f5c46f266756
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2025-01-11 19:44:36 -0400

    Añadiendo estado de mensajes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const listContacts = document.querySelector('.list-contact');
 const contentChat = document.querySelector('.content-chat-history');
+const loader = document.querySelector('.loader');
 
 const formMessage = document.querySelector('.frm-message');
 
@@ -9,6 +10,30 @@ const backUrl = ENVS.BACK_URL;
 const socketUrl = ENVS.SOCKET_URL;
 
 let contactFocus = null;
+
+function mostrarLoader( mostrar ){
+  const messagesLoader = [
+    'Cargando, por favor espere',
+    'Conectando...',
+    'Esto puede tardar un poco',
+    'Desventajas de tener el plan Free :('
+  ];
+
+  loader.style.display = (mostrar) ? 'flex' : 'none';
+
+  let index = 0;
+  loader.textContent =  messagesLoader[index];
+  const messageInterval = setInterval(() => {
+    if (!mostrar){
+      clearInterval(messageInterval);
+      return;
+    }
+    index++;
+    if(index == messagesLoader.length ) index = 0;
+    
+    loader.textContent =  messagesLoader[index];
+  }, 2000);
+}
 
 async function traerChat(phoneNumber){
   const chat = await fetch(`${backUrl}/api/whatsapp/chats/${phoneNumber}`)
@@ -23,7 +48,6 @@ async function traerListContact(){
 }
 
 function showListContacts(contacts){
-  console.log('Mostrando contacts');
   listContacts.innerHTML = '';
   contacts.forEach(contact => {
     const lastMessage = contact.lastMessage;
@@ -31,15 +55,22 @@ function showListContacts(contacts){
     const message = (lastMessage.of === 'me')
       ? lastMessage.text
       : `${contact.nameContact}: ${lastMessage.text}`;
+  
+    const timeMessage = transformTimestampToString( lastMessage.timestamp )
 
     const li = document.createElement('li');
     li.classList.add('contact');
     li.classList.remove('chat-selected');
     li.innerHTML = `
-      <div class="icon-contact"> M </div>
+      <div class="icon-contact">
+        <i class="fa-solid fa-user fa-2xl"></i>
+      </div>
       <div class="name-contact">
           <h2>${contact.nameContact}</h2>
-          <div>${message}</div>
+          <div class="view-message-contact">
+            <div class="msg-contact">${message}</div>
+            <span>${timeMessage}</span>
+          </div>
       </div>
     `;
 
@@ -53,9 +84,30 @@ function showListContacts(contacts){
   });
 }
 
+function showInfoMessage( messageHtml, status, timestamp, meTo ){
+  const timeMessage = transformTimestampToString( timestamp );
+  const divInfo = document.createElement('div');
+  divInfo.classList.add('content-info-message')
+  const timeInfo = document.createElement('span');
+  const statusInfo = document.createElement('span');
+
+  timeInfo.textContent = timeMessage;
+  divInfo.appendChild(timeInfo);
+
+  if(meTo == 'me'){
+    if(status == 'sent') statusInfo.innerHTML = `<i class="fa-solid fa-check"></i>`;
+    if(status == 'delivered') statusInfo.innerHTML = `<i class="fa-solid fa-check-double"></i>`;
+    if(status == 'read') statusInfo.innerHTML = `<i class="fa-solid fa-check-double" style="color: #74C0FC;"></i>`;
+
+    divInfo.appendChild(statusInfo);
+  }
+
+  messageHtml.appendChild(divInfo);
+}
+
 function showChat( messages ){
-  console.log('Mostrando chat');
   chatHtml.innerHTML = '';
+  moveScrollToBottomm( chatHtml );
   messages.forEach((m)=>{
     const contentMessage = document.createElement('div');
     contentMessage.classList.add('content-message');
@@ -71,6 +123,7 @@ function showChat( messages ){
     if(m.of === 'to') message.classList.add('message-contact');
 
     message.textContent = m.text;
+    showInfoMessage(message, m.status, m.timestamp, m.of);
     chatHtml.appendChild(contentMessage);
   });
 }
@@ -111,9 +164,26 @@ function connectToWebSockets() {
   };
 
   socket.onopen = ( event ) => {
+    mostrarLoader(false);
     console.log( 'Connected' );
   };
 }
+
+const transformTimestampToString = (timestamp, isHour12 = false) =>{
+  return new Date(timestamp * 1000).toLocaleString(undefined,{
+    hour12: isHour12,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
+
+const moveScrollToBottomm = ( component ) =>{
+  component.scrollTo({
+    top: component.scrollHeight
+  });
+}
+
+
 
 formMessage.addEventListener('submit', async(e)=>{
   e.preventDefault();
@@ -125,3 +195,4 @@ formMessage.addEventListener('submit', async(e)=>{
 })
 
 connectToWebSockets();
+mostrarLoader(true);
